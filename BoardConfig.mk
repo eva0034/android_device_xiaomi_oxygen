5ba2d2c0f9b4fe4e922056488a49b534912855b8
eva0034 <evans.john.96@outlook.com>
2018-07-09 10:52:38 +1000

    oxygen: enable real time lockscreen charging current values
    show realtime lockscreen charging current values

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,9 @@
 
 DEVICE_PATH := device/xiaomi/oxygen
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Kernel
 TARGET_KERNEL_CONFIG := hardrock_oxygen_defconfig
 
