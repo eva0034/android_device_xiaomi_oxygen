48c00b40addfaa3bd16af84b1035703aca1ed76f
Ayaanmalikbrt <43542602+Ayaanmalikbrt@users.noreply.github.com>
2018-11-10 09:30:09 +0530

    changes for havoc

--- mk_oxygen.mk
+++ mk_oxygen.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oxygen device
 $(call inherit-product, device/xiaomi/oxygen/device.mk)
 
-# Inherit some common mokeeOS stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit some common HavocOS stuff.
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := oxygen
-PRODUCT_NAME := mk_oxygen
+PRODUCT_NAME := havoc_oxygen
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi Max 2
 PRODUCT_MANUFACTURER := Xiaomi
